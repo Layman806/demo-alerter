e4bfbe05fd170281af6e84c3c449dbb49a16f54f
Layman806 <wakingbavani@gmail.com>
2024-01-30 19:15:35 +0530

    add authorization methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+  before_action :authorized
 
   SECRET_KEY = 'appsecretpass1234'
 
@@ -15,6 +16,19 @@ class ApplicationController < ActionController::API
       rescue JWT::DecodeError
         nil
       end
+    end
+  end
+
+  def current_user
+    if decoded_token
+      user_id = decoded_token[0]['user_id']
+      @user = User.find_by(id: user_id)
+    end
+  end
+
+  def authorized
+    unless !!current_user
+      render json: { message: 'Please log in' }, status: :unauthorized
     end
   end
 end
