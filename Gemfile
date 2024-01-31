f8ea27b76b5028be70def20bed166b39446feb20
Layman806 <wakingbavani@gmail.com>
2024-01-31 13:24:29 +0530

    add paging on alerts index with kaminari

--- Gemfile
+++ Gemfile
@@ -43,6 +43,8 @@ gem 'rack-cors'
 
 gem 'active_model_serializers', '~> 0.10.2'
 
+gem 'kaminari'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri windows ]
--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -1,5 +1,7 @@
 class AlertsController < ApplicationController
   def index
-    render json: Alert.where(user: current_user), status: 200
+    render json: Alert.where(user: current_user)
+                      .page(params[:page])
+                      .per(params[:per_page]), status: 200
   end
 end
