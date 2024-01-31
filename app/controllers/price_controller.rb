f94197a6ddf6826a4719d508dc2da5331ca514c9
Layman806 <wakingbavani@gmail.com>
2024-01-31 22:53:46 +0530

    refactor price controller

--- app/controllers/price_controller.rb
+++ app/controllers/price_controller.rb
@@ -2,23 +2,25 @@ class PriceController < ApplicationController
   def new
     sym = params[:sym]
     new_price = params[:price]
-    logger.info "\nNew price recvd -- Sym: #{sym}, Price: #{new_price}"
-    triggered_alerts(sym, new_price.to_d).each do |alert|
-      logger.info "\nAlert Triggered, id: #{alert.id}, user_id: #{alert.user_id}, trigger_price: #{alert.price}"
-    end
-  end
 
-  private
-
-  def triggered_alerts(sym, new_price)
     coin = Coin.find_by!(sym: sym)
     old_price = coin.price.to_d
     coin.price = new_price
     coin.save!
 
     logger.info "\nOld price: #{old_price}"
+    logger.info "\nNew price recvd -- Sym: #{sym}, Price: #{new_price}"
+
+    triggered_alerts(coin.id, old_price, new_price.to_d).each do |alert|
+      logger.info "\nAlert Triggered, id: #{alert.id}, user_id: #{alert.user_id}, trigger_price: #{alert.price}"
+    end
+  end
+
+  private
+
+  def triggered_alerts(coin_id, old_price, new_price)
     alerts = []
-    Alert.where(coin_id: coin.id).each do |alert|
+    Alert.where(coin_id: coin_id).each do |alert|
       next if %w[deleted triggered].include? alert[:status]
       if (old_price >= alert[:price] and new_price <= alert[:price]) or (old_price <= alert[:price] and new_price >= alert[:price])
         alerts.append(alert)
